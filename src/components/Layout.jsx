f446d4944d4616a76f759b925653cab7027be230
Timiano <timiajayi729@gmail.com>
2026-01-24 01:49:45 +0100

    Adjusted the Layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -20 }}
       transition={{ duration: 0.3 }}
-      className="flex-grow"
+      className="grow"
     >
       {children}
     </motion.main>

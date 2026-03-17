49e6fca662d48eff2bb9eeef539de6936840d014
Timiano <timiajayi729@gmail.com>
2026-03-17 14:19:19 +0100

    style: Fix z-index syntax in Preloader component for consistency

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -28,7 +28,7 @@ const Preloader = ({ onFinish }) => {
         initial={{ opacity: 1 }}
         exit={{ opacity: 0 }}
         transition={{ duration: 0.4 }}
-        className="fixed inset-0 z-[9999] flex items-center justify-center bg-white"
+        className="fixed inset-0 z-9999 flex items-center justify-center bg-white"
       >
         <div className="flex flex-col items-center">
           {/* Logo */}
